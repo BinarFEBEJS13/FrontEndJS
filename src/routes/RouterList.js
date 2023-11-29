5bb589a7bf7099140568cd8d3b080a052294bdbf
HannanAmar05 <112670911+HannanAmar05@users.noreply.github.com>
2023-11-29 20:14:10 +0700

    slicing Admin

--- src/routes/RouterList.js
+++ src/routes/RouterList.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { App } from "../pages/App";
+import Login from "../pages/Admin/Login";
+import Dashboard from "../pages/Admin/Dashboard";
+import { TableDashboard } from "../assets/components/TableDashboard";
+import { TableKelas } from "../assets/components/TableKelas";
 
 export const RouterList = () => {
   return (
@@ -9,6 +13,10 @@ export const RouterList = () => {
       <Routes>
         <Route path="/" element={<App />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/admin/login" element={<Login/>}></Route>
+        <Route path="/admin/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/admin/tabledashboard" element={<TableDashboard/>}></Route>
+        <Route path="/admin/tablekelas" element={<TableKelas/>}></Route>
       </Routes>
     </BrowserRouter>
   );
